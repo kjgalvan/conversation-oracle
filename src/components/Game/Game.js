d8d913bdfb502734e0af9290e1e24d826e2878e5
KJ <kaityagalvan@gmail.com>
2019-02-23 10:46:48 -0800

    Setup Game component

---
+++ src/components/Game/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Game.css';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="Game">
+
+      </div>
+    );
+  }
+}
+
+export default Game;
