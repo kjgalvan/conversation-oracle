64a67343ca9060aea913981490ca13906d0c7857
KJ <kaityagalvan@gmail.com>
2019-02-23 12:52:23 -0800

    Added Navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Game from '../Game/Game';
+import Navigation from '../Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Game />
-      </div>
+      <React.Fragment className="App">
+      <Navigation />
+      <Game />
+      </React.Fragment>
     );
   }
 }
